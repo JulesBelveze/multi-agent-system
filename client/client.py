e3317c52c33a073e0eb158f491bf1b7f349da272
otiosum <dimmy_khl@live.com>
2019-03-19 14:57:12 +0100

    arguments and client class

--- client/client.py
+++ client/client.py
@@ -1,11 +1,38 @@
+import sys
+from argparse import ArgumentParser
+
+def msg_server(message):
+    print(message, file=sys.stderr, flush=True)
+
+def msg_local(message):
+    print(message, file=sys.stdout, flush=True)
+
+class Client:
+    def __init__(self, server_args):
+        msg_local(server_args)
+
+    def solve_level(self):
+        msg_local("Solving level...")
+
 
 def main(args):
-    print("Hello world")
+    if args.level_name:
+        msg_local(args.level_name)
+        
+    # Read server messages from stdin.
+    server_messages = sys.stdin
+
+    # Use stderr to print to console through server.
+    msg_local("Poking starfish to life...")
+    startfish_client = Client(server_messages)
+
 
 if __name__ == "__main__":
-    #Process arguments
-    args = "temp argument"
+    # Process arguments
+    parser = ArgumentParser(description='Starfish client for solving transportation tasks.')
+    parser.add_argument('--level', dest="level_name", help='Provide path to level to run client locally', metavar="FILE")
 
-    #Run client
+    args = parser.parse_args()
+
+    # Run client
     main(args)
-    
