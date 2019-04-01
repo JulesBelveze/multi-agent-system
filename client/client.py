1dec478ee4c1b5302f586aa62bdff89a06c71ca7
JulesBelveze <jules.belveze@hotmail.fr>
2019-04-01 15:47:09 +0200

    add solve lovel and goal state to client

--- client/client.py
+++ client/client.py
@@ -7,6 +7,7 @@ from argparse import ArgumentParser
 def msg_server(message):
     print(message, file=sys.stdout, flush=True)
 
+
 def msg_error(message):
     print(message, file=sys.stderr, flush=True)
 
@@ -40,14 +41,20 @@ class SearchClient:
                 level.append(line)
                 row += 1
                 line = server_args.readline.rstrip()
+            line = server_args.readline.rstrip()
 
             self.max_row = row + 1
             self.max_col = max_col + 1
 
+            goal_level = []
+            while line != "#end":
+                goal_level.append(line)
+                line = server_args.readline.rstrip()
+
         except Exception:
             sys.exit()
 
-        self.initial_state = State() # TO BE CREATED
+        self.initial_state = State()  # TO BE CREATED
         self.walls = np.zeros((self.max_row, self.max_col), dtype=bool)
         self.goals = np.zeros((self.max_row, self.max_col), dtype=bool)
 
@@ -70,27 +77,70 @@ class SearchClient:
                 elif char == " ":
                     continue
                 else:
-                    msg_local("Error: unexpected character.")
+                    msg_error("Error: unexpected character.")
+                    sys.exit(1)
 
+        # looping through the goal level
+        self.goal_level = {}  # goal level is a dictionary containing boxes informations
+        for row, line in enumerate(goal_level):
+            for col, char in enumerate(row):
+                if char in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
+                    self.goal_level[char] = (row, col, color_dict[char])
+                elif char == "+" or char == " ":
+                    continue
+                else:
+                    msg_error("Error: unexepected character in goal state.")
+                    sys.exit(1)
 
-    def solve_level(self):
+    def solve_level(self, strategy):
         msg_error("Solving level...")
+        strategy.add_to_frontier(self.initial_state)
 
-    def print_solution(self):
-        msg_error("42")
+        iterations = 0
+        while True:
+            if iterations == 1000:
+                msg_error(strategy.search_status())
+                iterations = 0
+
+            if strategy.frontier_empty():
+                return None
+
+            leaf = strategy.get_and_remove_leaf()
+            if leaf.is_goal_state(self.goal_level):
+                return leaf.extract_plan()
+
+            strategy.add_to_explored(leaf)
+            for child_state in leaf.get_children(self.walls):
+                if not strategy.is_explored(child_state) and not strategy.in_frontier(child_state):
+                    strategy.add_to_frontier(child_state)
+
+            iterations += 1
+
 
 def main(args):
     msg_server("Starfish")
-    
+
     # Read server messages from stdin.
     server_messages = sys.stdin
 
     # Create client using server messages
-    starfish_client = Client(server_messages)
-    
+    starfish_client = SearchClient(server_messages)
+
+    strategy = None
+
     # Solve and print
-    starfish_client.solve_level()
-    starfish_client.print_solution()
+    solution = starfish_client.solve_level(strategy)
+    if solution is None:
+        msg_error("{}".format(strategy.search_status()))
+        msg_error("Unable to solve level.")
+
+    else:
+        msg_error("Found solution of length {}.".format(len(solution)))
+        msg_error("{}".format(strategy.search_status()))
+
+        # printing solution
+        for state in solution:
+            msg_error("{}".format(state.action))
 
 
 if __name__ == "__main__":
