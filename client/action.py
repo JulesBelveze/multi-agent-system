f35c018d3c9c36f39b2706eec3c313ffd8ed1d52
otiosum <dimmy_khl@live.com>
2019-05-03 21:30:04 +0200

    move waiting action to the end of the list

--- client/action.py
+++ client/action.py
@@ -52,7 +52,6 @@ class Action:
 ALL_ACTIONS = []
 
 # Populate
-ALL_ACTIONS.append(Action(ActionType.Wait, None, None))
 for agent_dir in (Direction.N, Direction.E, Direction.S, Direction.W):
     ALL_ACTIONS.append(Action(ActionType.Move, agent_dir, None))
 
@@ -60,3 +59,4 @@ for agent_dir in (Direction.N, Direction.E, Direction.S, Direction.W):
         for box_dir in (Direction.N, Direction.E, Direction.S, Direction.W):
             if agent_dir.d_row + box_dir.d_row != 0 or agent_dir.d_col + box_dir.d_col != 0:
                 ALL_ACTIONS.append(Action(action, agent_dir, box_dir))
+ALL_ACTIONS.append(Action(ActionType.Wait, None, None))
