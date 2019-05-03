709d4553cd0710314d555a809106d0a06bcd7b3f
otiosum <dimmy_khl@live.com>
2019-05-03 19:55:52 +0200

    start merging BIG algorithm into pathing/agent class

--- client/agent.py
+++ client/agent.py
@@ -1,17 +1,15 @@
-from pathing import PathingBestFirst
+from pathing import Pathing
 
 class Agent:
     def __init__(self, initial_state: 'State', agent_key: 'str'):
         self.agent_key = agent_key
         self.current_state = initial_state
-        self.goal_state = None
-        self.box_key = None
-        self.path_finder = PathingBestFirst()
+        self.path_finder = Pathing()
 
     def assign_goal(self, goal_state: 'State', box_key: 'str'):
         self.goal_state = goal_state
         self.box_key = box_key
-        self.path_finder.set_path_objective(goal_state, box_key, self.agent_key)
+        #self.path_finder.set_path_objective(goal_state, box_key, self.agent_key)
 
     def has_goal(self):
         if self.goal_state is None or self.box_key is None:
@@ -19,22 +17,31 @@ class Agent:
         return True
 
     def find_path_to_goal(self, walls):
-        # Start with initial state
-        self.path_finder.add_to_frontier(self.current_state)
+        agent = self.current_state.agents.get(self.agent_key)
+        c_box = self.current_state.boxes.get(self.box_key)
+        g_box = self.goal_state.boxes.get(self.box_key)
 
-        iterations = 0   #todo: this is a temp hack
-        while iterations < 15000:
-            if self.path_finder.frontier_count() == 0:
-                return None
-            
-            current = self.path_finder.get_from_frontier()
+        self.path_finder.calc_route(walls, (agent[0], agent[1]), (g_box[0], g_box[1]), self.current_state)
+        self.path_finder.print_path()
 
-            if current.is_goal_state(self.goal_state, self.box_key):
-                return current.extract_plan()
+
+        
+        # # Start with initial state
+        # self.path_finder.add_to_frontier(self.current_state)
+
+        # iterations = 0   #todo: this is a temp hack
+        # while iterations < 15000:
+        #     if self.path_finder.frontier_count() == 0:
+        #         return None
             
-            self.path_finder.add_to_explored(current)
-            for child_state in current.get_children(walls, self.agent_key, self.box_key):
-                if not self.path_finder.is_explored(child_state) and not self.path_finder.in_frontier(child_state):
-                    self.path_finder.add_to_frontier(child_state)
-            iterations += 1
+        #     current = self.path_finder.get_from_frontier()
+
+        #     if current.is_goal_state(self.goal_state, self.box_key):
+        #         return current.extract_plan()
+            
+        #     self.path_finder.add_to_explored(current)
+        #     for child_state in current.get_children(walls, self.agent_key, self.box_key):
+        #         if not self.path_finder.is_explored(child_state) and not self.path_finder.in_frontier(child_state):
+        #             self.path_finder.add_to_frontier(child_state)
+        #     iterations += 1
         return None
--- client/pathing.py
+++ client/pathing.py
@@ -1,100 +1,116 @@
-from abc import ABCMeta, abstractmethod
-from action import ActionType
+import numpy as np
 import heapq
+import copy
 
-class Heuristic:
-    def __init__(self, goal_state: 'State', box_key: 'str', agent_key: 'str'):
-        self.goal_state = goal_state
-        self.box_key = box_key
-        self.agent_key = agent_key
-
-    def h_get(self, walls, state: 'State') -> 'int':
-        agent = state.agents.get(self.agent_key)
-        # check if in a corridor
-        if walls[agent[0] - 1][agent[1]] == True and walls[agent[0] + 1][agent[1]] == True:
-            return self.h_tunnel(state)
-        elif walls[agent[0]][agent[1] - 1] == True and walls[agent[0]][agent[1] + 1] == True:
-            return self.h_tunnel(state)
-
-        return self.h_box_distance(state)
-
-    '''Moving boxes, uses manhattan distance between goal and current box'''
-    def h_box_distance(self, state: 'State') -> 'int':
-        goal_box = self.goal_state.boxes.get(self.box_key)
-        current_box = state.boxes.get(self.box_key)
-
-        #Manhattan distance
-        return abs(goal_box[0] - current_box[0]) + abs(goal_box[1] - current_box[1])
-
-    '''Moving agents, uses manhattan distance between goal and current agent'''
-    def h_agent_distance(self, state: 'State') -> 'int':
-        current_agent = state.agents.get(self.agent_key)
-        goal_agent = self.goal_state.boxes.get(self.box_key)
-
-        return abs(goal_agent[0] - current_agent[0]) + abs(goal_agent[1] - current_agent[1])
-
-    '''Potential heuristic for moving through 1 wide corridors'''
-    def h_tunnel(self, state: 'State') -> 'int':
-        
-        return 0
-
-
-class Pathing(metaclass=ABCMeta):
+class Pathing():
     def __init__(self):
-        self.explored = set()
-
-    def add_to_explored(self, state: 'State'):
-        self.explored.add(state)
-    
-    def is_explored(self, state: 'State') -> 'bool':
-        return state in self.explored
-
-    def explored_count(self) ->'int':
-        return len(self.explored)
-
-    @abstractmethod
-    def get_from_frontier(self) -> 'State': raise NotImplementedError
-    
-    @abstractmethod
-    def add_to_frontier(self, state: 'State'): raise NotImplementedError
-
-    @abstractmethod
-    def in_frontier(self, state: 'State') -> 'bool': raise NotImplementedError
-
-    @abstractmethod
-    def frontier_count(self) -> 'int': raise NotImplementedError
-
-
-class PathingBestFirst(Pathing):
-    def __init__(self):
-        super().__init__()
-        self.unique_increment = 0
+        self.closed = set()
+        self.open = set()
         self.frontier = []
-        self.frontier_set = set()
-        self.heuristic = None
         heapq.heapify(self.frontier)
 
-    def set_path_objective(self, goal_state: 'State', box_key: 'str', agent_key: 'str'):
-        self.heuristic = Heuristic(goal_state, box_key, agent_key)
+        self.val_grid = []
+        self.level_grid = []
 
-    def get_from_frontier(self):
-        leaf = heapq.heappop(self.frontier)[2]
-        self.frontier_set.remove(leaf)
-        return leaf
+    def calc_route(self, walls, start_index, end_index, state: 'State'):
+        lvl_rows = len(walls)
+        lvl_cols = len(walls[0])
 
-    def add_to_frontier(self, state):  
-        modifier = 1
-        if state.action is not None and state.action.action_type is ActionType.Wait:
-            modifier *= 2
-        heapq.heappush(self.frontier, (self.heuristic.h_box_distance(state) * modifier, self.unique_increment, state))
-        self.frontier_set.add(state)
-        self.unique_increment += 1
+        # Setup map
+        self.level_grid = copy.deepcopy(walls)
+        self.level_grid[end_index[0]][end_index[1]] = True
+        for key, box in state.boxes.items():
+            self.level_grid[box[0]][box[1]] = True
 
-    def in_frontier(self, state):
-        return state in self.frontier
+        for key, ag in state.agents.items():
+            self.level_grid[ag[0]][ag[1]] = True
 
-    def frontier_count(self):
-        return len(self.frontier)
+        # Setup value grid
+        self.val_grid = np.arange(lvl_rows * lvl_cols).reshape(lvl_rows, lvl_cols)
+        self.val_grid.fill(0000)
+
+        # Set value of destination and start cells
+        self.val_grid[end_index[0]][end_index[1]] = 1000
+        self.val_grid[start_index[0]][start_index[1]] = -1
+
+        # Map cells
+        self.calc_cells(end_index[0], end_index[1], start_index)
+
+    def calc_cells(self, v_index, h_index, start_index):
+        u = v_index - 1
+        d = v_index + 1
+        l = h_index - 1
+        r = h_index + 1
+        uh = False
+        dh = False
+        vr = False
+        vl = False
+
+        if self.val_grid[v_index][h_index] != 0:
+            if self.level_grid[u][h_index] == False:
+                self.val_grid[u][h_index] = self.val_grid[v_index][h_index] * 0.999
+                self.level_grid[u][h_index] = True
+                uh = True
+            if self.level_grid[d][h_index] == False:
+                self.val_grid[d][h_index] = self.val_grid[v_index][h_index] * 0.999
+                self.level_grid[d][h_index] = True
+                dh = True
+            if self.level_grid[v_index][r] == False:
+                self.val_grid[v_index][r] = self.val_grid[v_index][h_index] * 0.999
+                self.level_grid[v_index][r] = True
+                vr = True
+            if self.level_grid[v_index][l] == False:
+                self.val_grid[v_index][l] = self.val_grid[v_index][h_index] * 0.999
+                self.level_grid[v_index][l] = True
+                vl = True
+
+            if uh == True:
+                self.calc_cells(u, h_index, start_index)
+                uh = False
+            if dh == True:
+                self.calc_cells(d, h_index, start_index)
+                dh = False
+            if vr == True:
+                self.calc_cells(v_index, r, start_index)
+                vr = False
+            if vl == True:
+                self.calc_cells(v_index, l, start_index)
+                vl = False
+    
+    def print_path(self):
+        print(self.val_grid)
+
+
+# class PathingBestFirst(Pathing):
+#     def __init__(self):
+#         super().__init__()
+#         self.unique_increment = 0
+#         self.frontier = []
+#         self.frontier_set = set()
+#         self.heuristic = None
+#         heapq.heapify(self.frontier)
+
+#     def set_path_objective(self, goal_state: 'State', box_key: 'str', agent_key: 'str'):
+#         self.heuristic = Heuristic(goal_state, box_key, agent_key)
+
+#     def get_from_frontier(self):
+#         leaf = heapq.heappop(self.frontier)[2]
+#         self.frontier_set.remove(leaf)
+#         return leaf
+
+#     def add_to_frontier(self, state):  
+#         modifier = 1
+#         if state.action is not None and state.action.action_type is ActionType.Wait:
+#             modifier *= 2
+#         heapq.heappush(self.frontier, (self.heuristic.h_box_distance(state) * modifier, self.unique_increment, state))
+#         self.frontier_set.add(state)
+#         self.unique_increment += 1
+
+#     def in_frontier(self, state):
+#         return state in self.frontier
+
+#     def frontier_count(self):
+#         return len(self.frontier)
 
     # Printing functions for debug purposes
     #todo
