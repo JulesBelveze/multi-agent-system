070cbaf80fef3676dba81d99ae5cd0034c04c990
JulesBelveze <jules.belveze@hotmail.fr>
2019-05-06 12:57:27 +0200

    assigning each goal to its respective agent

--- client/client.py
+++ client/client.py
@@ -11,6 +11,7 @@ from message import msg_server_err
 from message import msg_server_comment
 from message import msg_server_action
 
+
 class Client:
     def __init__(self, server_args):
         try:
@@ -97,10 +98,15 @@ class Client:
         # TODO: this part needs extensive overhaul to account for several agents
 
         solutions = []
-        for char in self.goal_state.boxes.keys():
-            self.agents[0].assign_goal(self.goal_state, char)
 
-            result = self.agents[0].find_path_to_goal(self.walls)
+        for char, value in self.goal_state.boxes.items():
+            # assigning each goal to an agent by looking at the box color
+            _, _, box_color = value
+            key_agent = [x for x, y in self.initial_state.agents.items() if y[2] == box_color][0]
+            key_agent = int(key_agent)
+
+            self.agents[key_agent].assign_goal(self.goal_state, char)
+            result = self.agents[key_agent].find_path_to_goal(self.walls)
             if result is None:
                 return None
             solutions.append(result)
