eb9c8b0e977f1f6fffce41fb501d9cdcce4d69f8
otiosum <dimmy_khl@live.com>
2019-05-14 23:53:22 +0200

    squash bugs and simplify

--- client/agent.py
+++ client/agent.py
@@ -22,9 +22,6 @@ class Agent:
             return False
         return True
 
-    def is_agent_next_to_box(self, agent, box):
-        return abs(box[0] - agent[0]) + abs(box[1] - agent[1]) == 1
-
     def pick_h_target(self, agent, box, nav_step):
         return agent if nav_step == 0 else box
 
@@ -40,8 +37,9 @@ class Agent:
             msg_server_comment("Found path from agent {} to box {}".format(self.agent_key, self.box_key))
 
             iterations = 0
+            h_target = agent
             for nav_step in range(0, 2):
-                self.navigator.add_to_frontier(self.current_state, self.navigator.h_calculate(self.pick_h_target(agent, c_box, nav_step), path))
+                self.navigator.add_to_frontier(self.current_state, self.navigator.h_calculate(h_target, path))
                 while iterations < 16000:
                     if self.navigator.frontier_count() == 0:
                         msg_server_err("Failed to navigate agent {} to box {}!".format(self.agent_key, self.box_key))
@@ -51,14 +49,15 @@ class Agent:
                     agent = current.agents.get(self.agent_key)
 
                     # Goal checking
-                    goal_check = False
                     if nav_step == 0:
-                        goal_check = self.is_agent_next_to_box(agent, c_box)
+                        is_sub_goal = abs(c_box[0] - agent[0]) + abs(c_box[1] - agent[1]) == 1
+                        h_target = agent
                     else:
                         c_box = current.boxes.get(self.box_key[0])[self.box_key[1]]
-                        goal_check = self.navigator.h_calculate(c_box, path) - self.navigator.h_calculate(g_box, path) == 0
+                        is_sub_goal = self.navigator.h_calculate(c_box, path) - self.navigator.h_calculate(g_box, path) == 0
 
-                    if goal_check:
+                    if is_sub_goal:
+                        is_sub_goal = False
                         final_plan = current.extract_plan()
                         self.current_state = current
                         self.navigator = Navigate()
@@ -69,7 +68,8 @@ class Agent:
                     self.navigator.add_to_explored(current)
                     for child_state in current.get_children(walls, self.agent_key, self.box_key):
                         if not self.navigator.is_explored(child_state) and not self.navigator.in_frontier(child_state):
-                            h_target = self.pick_h_target(agent, child_state.boxes.get(self.box_key[0])[self.box_key[1]], nav_step)
+                            if nav_step > 0:
+                                h_target = child_state.boxes.get(self.box_key[0])[self.box_key[1]]
                             self.navigator.add_to_frontier(child_state, self.navigator.h_calculate(h_target, path))
 
                     iterations += 1
