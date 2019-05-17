fb0d5dd80d0f7bbde3bc6689a1fc4a14ba0c8ecd
otiosum <dimmy_khl@live.com>
2019-05-17 12:43:45 +0200

    rework action building to how they implemented them in server

--- client/action.py
+++ client/action.py
@@ -59,6 +59,12 @@ for agent_dir in (Direction.N, Direction.E, Direction.S, Direction.W):
 
     for action in (ActionType.Push, ActionType.Pull):
         for box_dir in (Direction.N, Direction.E, Direction.S, Direction.W):
-            if agent_dir.d_row + box_dir.d_row != 0 or agent_dir.d_col + box_dir.d_col != 0:
-                ALL_ACTIONS.append(Action(action, agent_dir, box_dir))
+            if action is ActionType.Push:
+                # If not opposite directions
+                if agent_dir.d_row + box_dir.d_row != 0 or agent_dir.d_col + box_dir.d_col != 0:
+                    ALL_ACTIONS.append(Action(action, agent_dir, box_dir))
+            else:
+                # If not the same directions
+                if agent_dir is not box_dir:
+                    ALL_ACTIONS.append(Action(action, agent_dir, box_dir))
 ALL_ACTIONS.append(Action(ActionType.NoOp, None, None))
