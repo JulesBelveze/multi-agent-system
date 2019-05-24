6b5001ad8495ff39bb2ed3387af1ab0b599f3b09
otiosum <dimmy_khl@live.com>
2019-05-25 01:37:22 +0200

    remove gui limited since its useless, fix dir testing

--- flexi_test.py
+++ flexi_test.py
@@ -9,8 +9,9 @@ gui_command = "java -jar server.jar -c \"py client/client.py\" -l \"{}\" -g -t {
 def scan_dir_for_levels(dir_name, recursive_dir):
     levels = []
     keyword = '**/*.lvl' if recursive_dir else '*.lvl'
-    for f_name in Path(dir_name).glob(keyword):
-        levels.append(str(f_name.name))
+
+    for f in Path(dir_name).glob(keyword):
+        levels.append(str(f))
     return levels
 
 def find_level(level_name):
@@ -24,18 +25,13 @@ def format_log_name(level_path):
     name = level_path.replace("levels", "", 1).replace(slash, "-").replace(".lvl","")
     return "[{}]log.txt".format(name[1:])
 
-def test_levels(levels, gui, gui_limiter, timeout):
-    limiter_count = 0
+def test_levels(levels, is_dir, gui, timeout):
     for lvl in levels:
-        lvl_path = find_level(lvl)
+        lvl_path = lvl if is_dir else find_level(lvl)
         if lvl_path is not None:
             print("Testing: {}".format(lvl_path))
 
-            cmd = command
-            if gui and limiter_count < gui_limiter:
-                cmd = gui_command
-                limiter_count += 1
-
+            cmd = gui_command if gui else command
             p = Popen(cmd.format(lvl_path, timeout), stdout=PIPE, stderr=STDOUT)
 
             log_file = format_log_name(lvl_path)
@@ -61,11 +57,13 @@ def test_levels(levels, gui, gui_limiter, timeout):
 def main(args):
     if args.lvls is not None:
         levels = args.lvls.split(',')
-        test_levels(levels, args.g, args.madgui, args.t)
+        test_levels(levels, False, args.g, args.t)
     elif args.dir is not None:
         levels = scan_dir_for_levels(args.dir, args.recur)
         if len(levels) > 0:
-            test_levels(levels, args.g, args.madgui, args.t)
+            test_levels(levels, True, args.g, args.t)
+        else:
+            print("No levels found in directory")
     else:
         print("Did not supply level or directory arguments. Use '-h' for help")
 
@@ -74,11 +72,10 @@ if __name__ == "__main__":
     # Process arguments
     parser = ArgumentParser(description='Mass testing utility for Starfish')
     parser.add_argument('-lvls', default=None, help='Provide name of levels separated by comma. Individual levels also supported')
-    parser.add_argument('-dir', default=None, help='Test all levels inside given directory. Directory must be inside levels folder')
+    parser.add_argument('-dir', default=None, help='Test all levels inside given directory. Directory must be inside levels folder. E.g.: levels, levels/test')
     parser.add_argument('-recur', default=False, help='Set to true to also test files in sub-directories of given directory')
-    parser.add_argument('-g', default=False, help='Set to true to also run the server graphics. Limited to 1 level by default.')
-    parser.add_argument('-madgui', default=1, type=int, help='Set the gui limit to whatever and make your machine suffer! Use at your own risk')
-    parser.add_argument('-t', default=10, type=int, help='Set the timeout for the client in seconds. Default: 60')
+    parser.add_argument('-g', default=False, help='Set to true to also run the server graphics. Opens 1 at a time and must be closed manually')
+    parser.add_argument('-t', default=60, type=int, help='Set the timeout for the client in seconds. Default: 60')
     args = parser.parse_args()
 
     # Run client
